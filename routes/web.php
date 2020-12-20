a1e9e23fba0bb66da0379682866903846faa4ffe
Edson Rodrigues <edsssonrodrigues@gmail.com>
2020-12-20 14:38:25 -0200

    CHANGE | Adding all product routes to the same prefix.

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,22 @@
 <?php
 
-Route::prefix('/produtos')->group(function () {
+Route::prefix('produtos')->group(function () {
     Route::get('', 'ProdutoController@index')->name('lista_produtos');
     Route::get('/create', 'ProdutoController@create')->name('form_cria_produto');
     Route::get('/{id}', 'ProdutoController@show')->name('exibe_produto');
     Route::post('', 'ProdutoController@store')->name('cria_produto');
+    Route::get('/{id}/edit', 'ProdutoController@edit')->name('form_edita_produto');
+    Route::put('/{id}', 'ProdutoController@update')->name('edita_produto');
+    Route::delete('/{id}', 'ProdutoController@destroy')->name('remove_produto');
 });
 
 // Route::get('/produtos', 'ProdutoController@index')->name('lista_produtos');
 // Route::get('/produtos/create', 'ProdutoController@create')->name('form_cria_produto');
 // Route::get('/produtos/{id}', 'ProdutoController@show')->name('exibe_produto');
 // Route::post('/produtos', 'ProdutoController@store')->name('cria_produto');
-Route::get('/produtos/{id}/edit', 'ProdutoController@edit')->name('form_edita_produto');
-Route::put('/produtos/{id}', 'ProdutoController@update')->name('edita_produto');
-Route::delete('/produtos/{id}', 'ProdutoController@destroy')->name('remove_produto');
+// Route::get('/produtos/{id}/edit', 'ProdutoController@edit')->name('form_edita_produto');
+// Route::put('/produtos/{id}', 'ProdutoController@update')->name('edita_produto');
+// Route::delete('/produtos/{id}', 'ProdutoController@destroy')->name('remove_produto');
 
 Route::get('/categorias', 'CategoriaController@index')->name('lista_categorias');
 Route::get('/categorias/create', 'CategoriaController@create')->name('form_cria_categoria');
