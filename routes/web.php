5f0509079a3a559c78cae1c5c60675a7585d46f9
Edson Rodrigues <edsssonrodrigues@gmail.com>
2020-12-20 14:42:34 -0200

    CHANGE | Adding all category routes to the same prefix.

--- routes/web.php
+++ routes/web.php
@@ -10,17 +10,11 @@ Route::prefix('produtos')->group(function () {
     Route::delete('/{id}', 'ProdutoController@destroy')->name('remove_produto');
 });
 
-// Route::get('/produtos', 'ProdutoController@index')->name('lista_produtos');
-// Route::get('/produtos/create', 'ProdutoController@create')->name('form_cria_produto');
-// Route::get('/produtos/{id}', 'ProdutoController@show')->name('exibe_produto');
-// Route::post('/produtos', 'ProdutoController@store')->name('cria_produto');
-// Route::get('/produtos/{id}/edit', 'ProdutoController@edit')->name('form_edita_produto');
-// Route::put('/produtos/{id}', 'ProdutoController@update')->name('edita_produto');
-// Route::delete('/produtos/{id}', 'ProdutoController@destroy')->name('remove_produto');
-
-Route::get('/categorias', 'CategoriaController@index')->name('lista_categorias');
-Route::get('/categorias/create', 'CategoriaController@create')->name('form_cria_categoria');
-Route::post('/categorias', 'CategoriaController@store')->name('cria_categoria');
-Route::get('/categorias/{id}/edit', 'CategoriaController@edit')->name('form_edita_categoria');
-Route::patch('/categorias/{id}', 'CategoriaController@update')->name('edita_categoria');
-Route::delete('/categorias/{id}', 'CategoriaController@destroy')->name('remove_categoria');
+Route::prefix('categorias')->group(function () {
+    Route::get('', 'CategoriaController@index')->name('lista_categorias');
+    Route::get('/create', 'CategoriaController@create')->name('form_cria_categoria');
+    Route::post('', 'CategoriaController@store')->name('cria_categoria');
+    Route::get('/{id}/edit', 'CategoriaController@edit')->name('form_edita_categoria');
+    Route::patch('/{id}', 'CategoriaController@update')->name('edita_categoria');
+    Route::delete('/{id}', 'CategoriaController@destroy')->name('remove_categoria');
+});
